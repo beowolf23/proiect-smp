1e42b51464c34efe6813a3021abc32cc295c477b
BiancaBarbieru <94743752+BiancaBarbieru@users.noreply.github.com>
2023-05-23 19:10:33 +0300

    bluetooth

--- proiect_SMP/proiect_SMP.ino
+++ proiect_SMP/proiect_SMP.ino
@@ -3,55 +3,118 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <SoftwareSerial.h>
 
 #define DHTPIN 7  // Replace with the digital pin connected to the DHT11 sensor
 #define DHTTYPE DHT11
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+// interfaces
+SoftwareSerial bluetooth(8, 9);  // Definește pini pentru conexiunea Bluetooth (TX, RX)
 DHT dht(DHTPIN, DHTTYPE);
-Adafruit_BMP280 bmp; 
-
+Adafruit_BMP280 bmp;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-void setup() {
-	Serial.begin(9600);
+// variables
+float temperature;
+float humidity;
+float pressure;
+float altitude;
+volatile bool type;
+String current_command;
+String commands[2] = {"C1", "C2"};
 
-  bmp.begin(0x76);
-  delay(2000);
-
-  lcd.begin(16, 2);
-  delay(2000);
-  dht.begin();
+void handleBTInterrupt() {
+  Serial.println(current_command);
 }
 
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  bmp.begin(0x76);
+  lcd.begin(16, 2);
+  dht.begin();
+
+  attachInterrupt(digitalPinToInterrupt(8), handleBTInterrupt, CHANGE);
+
+  current_command = "";
+  type = 0;
+}
+
+
 void loop() {
+
+  // // reads bluetooth data
+  if(bluetooth.available())
+  {
+    String data = bluetooth.readString();
+    if(data.equals('C1')) {
+      current_command = data;
+      handleBTInterrupt();
+      Serial.print("AM primit C1");
+    }
+
+    // Serial.print(data);
+  }
+
+  lcd.clear();
+
   // Read temperature and humidity from DHT11
-  // float temperature = dht.readTemperature();
-  // float humidity = dht.readHumidity();
+  // Display temperature and humidity on the LCD
+  if (type == 0) {
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temperature);
+    lcd.print("C");
 
-  // // Display temperature and humidity on the LCD
-  // lcd.setCursor(0, 0);
-  // lcd.print("Temp: ");
-  // lcd.print(temperature);
-  // lcd.print(" C");
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity: ");
+    lcd.print(humidity);
+    lcd.print("%");
 
-  // lcd.setCursor(0, 1);
-  // lcd.print("Humidity: ");
-  // lcd.print(humidity);
-  // lcd.print(" %");
+    delay(3000);
+  } else {
+    temperature = bmp.readTemperature();
+    pressure = bmp.readPressure() / 100.0F;
+    altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temperature);
+    lcd.print("C");
 
-  	Serial.print("Temperature = ");
-	Serial.print(bmp.readTemperature());
-	Serial.println("*C");
+    lcd.setCursor(0, 1);
+    lcd.print("Height: ");
+    lcd.print(altitude);
+    lcd.print("m");
 
-	Serial.print("Pressure = ");
-	Serial.print(bmp.readPressure() / 100.0F);
-	Serial.println("hPa");
+    delay(3000);
+    lcd.clear();
 
-	Serial.print("Approx. Altitude = ");
-	Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-	Serial.println("m");
+    lcd.setCursor(0, 0);
+    lcd.print("Pres: ");
+    lcd.print(pressure);
+    lcd.print("hPa");
 
-	Serial.println();
-	delay(1000);
+    delay(3000);
+  }
+
+  type = !type;
+
+
+  // 	Serial.print("Temperature = ");
+  // Serial.print(bmp.readTemperature());
+  // Serial.println("*C");
+
+  // Serial.print("Pressure = ");
+  // Serial.print(bmp.readPressure() / 100.0F);
+  // Serial.println("hPa");
+
+  // Serial.print("Approx. Altitude = ");
+  // Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  // Serial.println("m");
+
+  // Serial.println();
+  // delay(1000);
 }
